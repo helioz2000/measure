29c24e0770fea05720b0aa9b7e677b0339903372
helioz2000 <helioz2000@gmail.com>
2019-12-04 16:32:50 +1100

    Update mqtt.cpp

--- mqtt.cpp
+++ mqtt.cpp
@@ -111,7 +111,7 @@ static void on_subscribe(struct mosquitto *mosq, void *obj, int mid, int qos_cou
      mosquitto_lib_init();
      int major, minor, revision, result;
      result = mosquitto_lib_version(&major, &minor, &revision);
-     printf("%s - mosquitto library V%d.%d.%d (%d)\n", __func__, major, minor, revision, result);
+     printf("%s - mosquitto library V%d.%d.%d (%d)\n", __PRETTY_FUNCTION__, major, minor, revision, result);
      syslog(LOG_INFO, "mosquitto library V%d.%d.%d (%d)", major, minor, revision, result);
 
      // create new mqtt
